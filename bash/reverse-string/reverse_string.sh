472f73437b869b411a193e445909221d57fd314b
kzk0829 <kzk0829kop@gmail.com>
2021-01-07 22:58:28 +0900

    reverse string done

---
+++ bash/reverse-string/reverse_string.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+  main () {
+    copy=$1
+    len=${#copy}
+
+    for((i=$len-1;i>=0;i--)); do rev="$rev${copy:$i:1}"; done
+
+    echo $rev
+  }
+
+  # call main with all of the positional arguments
+  main "$1"
