995c5a0217dd81de0b6088e784f829b68c2f79be
kzk0829 <kzk0829kop@gmail.com>
2021-01-09 19:21:21 +0900

    two-fer solve

---
+++ bash/two-fer/two_fer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+  main () {
+    name=$1
+
+    if [ $# = 0 ]; then
+      name="you"
+    fi
+
+    echo "One for $name, one for me."
+  }
+
+  # call main with all of the positional arguments
+  main "$@"
+
