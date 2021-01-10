11051c0f1d6e6568f97780a297611997c0980886
kzk0829 <kzk0829kop@gmail.com>
2021-01-10 16:35:33 +0900

    solve resisitor_color_duo

---
+++ bash/resistor-color-duo/resistor_color_duo.sh
@@ -0,0 +1,29 @@
+#!/usr/local/bin/bash
+
+  main () {
+    
+    declare -A colors
+
+    colors=(
+      ["Black"]="0"
+      ["Brown"]="1"
+      ["Red"]="2"
+      ["Orange"]="3"
+      ["Yellow"]="4"
+      ["Green"]="5"
+      ["Blue"]="6"
+      ["Violet"]="7"
+      ["Grey"]="8"
+      ["White"]="9"
+    )
+
+    if [ -z "${colors["${1^}"]}" ] || [ -z "${colors["${2^}"]}" ]; then
+      echo "invalid color"
+      exit 1
+    else
+      echo "${colors["${1^}"]}${colors["${2^}"]}"
+    fi
+  }
+
+  # call main with all of the positional arguments
+  main "$@"
